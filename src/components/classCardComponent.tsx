7a408da71550ea152479be5bc11b41c30eea59b5
Bruno Tavares <brunobtavares@outlook.com>
2023-11-07 19:09:20 -0300

    Ajustes

--- src/components/classCardComponent.tsx
+++ src/components/classCardComponent.tsx
@@ -23,9 +23,28 @@ export default function ClassCardComponent({ item }: ClassCardComponentProps) {
         if (isNaN(numericAbsence)) { return ''; }
         if (isNaN(numericTeachingHours)) { return ''; }
 
+        const max = 25;
+        const min = 0;
+
         const percent = (numericAbsence / numericTeachingHours) * 100;
 
-        return percent == 25 ? 'text-warning' : percent > 25 ? 'text-danger' : '';
+        // Garante que o valor esteja dentro do intervalo permitido (0 a 25)
+        const adjustedValue = Math.min(max, Math.max(min, percent));
+
+        switch (true) {
+            case adjustedValue === 0:
+                return "#008000";
+            case adjustedValue >= 1 && adjustedValue <= 5:
+                return "#339933";
+            case adjustedValue >= 6 && adjustedValue <= 10:
+                return "#FFDD00";
+            case adjustedValue >= 11 && adjustedValue <= 20:
+                return "#FF6600";
+            case adjustedValue >= 21 && adjustedValue <= 25:
+                return "#CC0000";
+            default:
+                return "rgba(255, 255, 255, 0.87)"
+        }
     }
 
     return (
@@ -46,7 +65,7 @@ export default function ClassCardComponent({ item }: ClassCardComponentProps) {
                     </div>
                     <div>
                         <h5>Faltas</h5>
-                        <h6 className={defineAbsenceColor(item.teachingHours, item.absence)}>{item.absence}</h6>
+                        <h6 style={{ color: defineAbsenceColor(item.teachingHours, item.absence) }}>{item.absence}</h6>
                     </div>
                     <div>
                         <h5>Presenças</h5>
