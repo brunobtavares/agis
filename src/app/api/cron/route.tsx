aa5068ea4b05df16caed6871bc9edb1f99b5b3b5
Bruno Tavares <brunobtavares@outlook.com>
2024-03-01 12:40:19 -0300

    Melhorias

--- src/app/api/cron/route.tsx
+++ src/app/api/cron/route.tsx
@@ -1,6 +1,9 @@
+import { Api } from "@/axios/client";
+
 export async function GET() {
     console.log('Running cron job...');
-    return new Response('CRON: Check API', {
-        status: 200
-    });
+
+    Api.get('/').then(response => console.log('Sucesso'), err => console.log('Erro'));
+
+    return new Response('CRON: Check API', { status: 200 });
 }
